bf8a97672e49baaa0b98c2b2cdb89b637607a368
Lruquaf <yavuzselimtuncer02@gmail.com>
2022-11-17 22:15:11 +0300

    first commit

--- scripts/deploy_staking.py
+++ scripts/deploy_staking.py
@@ -4,13 +4,14 @@ from scripts.helpful_scripts import get_account
 
 def deploy():
     account = get_account()
-    lending = Staking.deploy(
-        config["networks"][network.show_active()]["usdt_token"],
+    staking = Staking.deploy(
+        config["networks"][network.show_active()]["link_token"],
         config["networks"][network.show_active()]["link_usdt_price_feed"],
+        10,
         {"from": account},
         publish_source=config["networks"][network.show_active()]["verify"],
     )
-    print(f"Deployed at {lending.address}")
+    print(f"Deployed at {staking.address}")
 
 
 def main():
